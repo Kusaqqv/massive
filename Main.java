e9f9d8ba417df99a72fe571f4543a417b115df4c
Kusaqqv <andrei.salov.05@mail.ru>
2023-12-22 22:49:12 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+        massive(array);
+        System.out.println("Отсортированный массив:");
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void massive(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
